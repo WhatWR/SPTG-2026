72a8a0a482a786b52913744d24f86983eec975d2
WhatWR <kul.boonanake@gmail.com>
2026-04-17 23:50:53 +0700

    a

---
+++ src/routes/middle-east/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+  <title>Middle East | SPTG Nexus</title>
+</svelte:head>
+
+<iframe
+  src="/guides/Middle_East_Study_Guide.html"
+  title="Middle East Current Affairs Guide"
+  class="full-iframe"
+  allowfullscreen
+></iframe>
+
+<style>
+  .full-iframe {
+    width: 100%;
+    height: 100%;
+    border: none;
+    box-sizing: border-box;
+    display: block;
+    background: #060913;
+  }
+</style>
